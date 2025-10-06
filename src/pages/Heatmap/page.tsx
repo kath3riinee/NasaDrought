cd4871ff69a882bfff2fff80adcf0eaa74d98c75
kath3riinee <katherine.shaw08@gmail.com>
2025-10-06 00:09:47 -0400

    update Heatmap path fix

--- src/pages/Heatmap/page.tsx
+++ src/pages/Heatmap/page.tsx
@@ -7,7 +7,7 @@ export default function HeatmapPage() {
       <h1 style={{ padding: "1rem", margin: 0 }}>Heatmap - Please give at least 1-2 minutes to load if needed</h1>
       <div style={{ flex: 1 }}>
         <iframe
-          src={`${process.env.PUBLIC_URL}/drought_map.html`}
+          src={`${window.location.origin}/NasaDrought/drought_map.html`}
           title="Drought Heatmap"
           style={{ width: "100%", height: "100%", border: "0" }}
         />
